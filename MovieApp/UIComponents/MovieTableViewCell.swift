086b1b978e8b2e629b55ef71bd1b78de33b10905
Furqan <furqan@furqans-imac.local>
2017-03-23 22:02:20 +0500

    UI Implementation of Search View

---
+++ MovieApp/UIComponents/MovieTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieTableViewCell.swift
+//  MovieApp
+//
+//  Created by Furqan on 3/23/17.
+//  Copyright © 2017 Furqan. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imgViewPoster: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblReleaseDate: UILabel!
+    @IBOutlet weak var textviewOverview: UITextView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieApp/ViewControllers/HomeViewController.swift
+++ MovieApp/ViewControllers/HomeViewController.swift
@@ -8,12 +8,79 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+
+enum DisplayMode: Int {
+    case searchResults, suggestions
+}
+
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    
+    @IBOutlet var tableviewMovies: UITableView!
+    @IBOutlet var searchBar: UISearchBar!
+    
+    
+    
+    //MARK:- Private Methods
+    
+    func updateResults() {
+        
+        refreshControl.endRefreshing()
+        
+    }
+    
+    
+    private func setupPullToRefresh() {
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(self.updateResults), for: UIControlEvents.valueChanged)
+        tableviewMovies.addSubview(refreshControl)
+    }
+    
+    //Pull to refresh - update data
+    var refreshControl: UIRefreshControl!
+    
+    var currentDisplayMode = DisplayMode.searchResults
+    
+    
+    //MARK:- UITableView Datasource/Delegate
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+//        cell.textLabel?.text = "Movie"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieTableViewCell
+        return cell
+    }
+    
+    
+    //MARK:- UISearchBar Delegate
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchBar.resignFirstResponder()
+        
+    }
+    
+    //MARK:- Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupPullToRefresh()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableviewMovies.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
